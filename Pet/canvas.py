0749ca335673554927ee71444e9972f568ec163c
Saketh Renangi <rensaketh@gmail.com>
2022-03-27 17:39:51 -0400

    Fixed errors in canvas.py
    Fixed errors when pulling canvas courses

--- Pet/canvas.py
+++ Pet/canvas.py
@@ -2,12 +2,14 @@ from canvasapi import Canvas
 
 API_URL = "https://ufl.instructure.com"
 
-API_KEY = "insertKeyHere"
+API_KEY = "insertKey"
 
 canvas = Canvas(API_URL, API_KEY)
 
 courseList = canvas.get_courses()
 print(courseList[0])
 
-course = canvas.get_course(452971)
-print(course.name)
+
+for i in courseList:
+    if not(hasattr(i, 'access_restricted_by_date')):
+        print(i)
