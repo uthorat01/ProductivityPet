d9b382ee180d6cd9f40ba420d3f3357b607ac169
Saketh Renangi <rensaketh@gmail.com>
2022-04-05 15:34:48 -0400

    Update canvas.py
    Fetched valid assignments and current courses

--- Pet/canvas.py
+++ Pet/canvas.py
@@ -1,5 +1,8 @@
 from canvasapi import Canvas
 
+from datetime import datetime
+import pytz
+
 API_URL = "https://ufl.instructure.com"
 
 API_KEY = "insertKey"
@@ -7,9 +10,18 @@ API_KEY = "insertKey"
 canvas = Canvas(API_URL, API_KEY)
 
 courseList = canvas.get_courses()
-print(courseList[0])
-
 
 for i in courseList:
     if not(hasattr(i, 'access_restricted_by_date')):
-        print(i)
+
+        if(i.enrollment_term_id == 2081):
+            course = canvas.get_course(i.id)
+            print("\n" + course.course_code + "\n")
+            assignmentList = course.get_assignments()
+            for assignment in assignmentList:
+                #present = datetime.now()
+                present = pytz.UTC.localize(datetime.now())
+                if(assignment.due_at != None and present < assignment.due_at_date):
+                    print(assignment)
+
+    #get classes, assignments, and periods
